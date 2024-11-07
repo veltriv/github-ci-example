f2f5591449961ae5c7266cf443ebbc05bb11c725
Vincent Veltri <vveltri@instructors.2u.com>
2024-11-06 19:09:55 -0500

    render

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/tests/setup.js',
+  },
   plugins: [react()],
 })
